ec2ad209213344dcfadcb4b986475c53195188fc
Kyle DeGuzman <kyle.deguzman.579@my.csun.edu>
2022-02-11 09:18:32 -0800

    collect monthly data

--- index.js
+++ index.js
@@ -4,7 +4,17 @@ const express = require("express");
 
 request("https://en.wikipedia.org/wiki/List_of_most-streamed_artists_on_Spotify", (error, response, html) =>{
   if(!error && response.statusCode == 200){
-    console.log("success");
+    const $ = cheerio.load(html);
+    let stats = [];
+
+    $(".mw-body .mw-body-content .mw-parser-output .wikitable tbody td a").each((i,el) => {
+      const item = $(el).text().replace(/[0-9]/g, '').replace(/[\[\]']+/g,'');
+      if(stats.length < 20 && item != ""){
+        stats.push(item);
+      }
+      //console.log(i + ": " + item);
+    })
+    console.log(stats);
   }
   else{
     console.log("error")
