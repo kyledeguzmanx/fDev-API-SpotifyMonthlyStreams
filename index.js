515603ccd9181cbc71116328cc8cb2f884add6ce
Kyle DeGuzman <kyle.deguzman.579@my.csun.edu>
2022-02-11 09:18:02 -0800

    request success

--- index.js
+++ index.js
@@ -1,3 +1,12 @@
 const request = require("request");
 const cheerio = require("cheerio");
 const express = require("express");
+
+request("https://en.wikipedia.org/wiki/List_of_most-streamed_artists_on_Spotify", (error, response, html) =>{
+  if(!error && response.statusCode == 200){
+    console.log("success");
+  }
+  else{
+    console.log("error")
+  }
+})
