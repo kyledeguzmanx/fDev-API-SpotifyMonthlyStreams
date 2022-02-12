8b60b4e8827e76ae44fb828f7a90b7cf5f94f443
Kyle DeGuzman <kyle.deguzman.579@my.csun.edu>
2022-02-11 22:51:34 -0800

    retrieved monthly data

--- index.js
+++ index.js
@@ -4,17 +4,13 @@ const express = require("express");
 
 request("https://en.wikipedia.org/wiki/List_of_most-streamed_artists_on_Spotify", (error, response, html) =>{
   if(!error && response.statusCode == 200){
+    
     const $ = cheerio.load(html);
     let stats = [];
+    let numbers = [];
+    const newsfeed = $(".mw-body .mw-body-content .mw-parser-output .wikitable tbody td a");
+    //console.log(newsfeed.html());
 
-    $(".mw-body .mw-body-content .mw-parser-output .wikitable tbody td a").each((i,el) => {
-      const item = $(el).text().replace(/[0-9]/g, '').replace(/[\[\]']+/g,'');
-      if(stats.length < 20 && item != ""){
-        stats.push(item);
-      }
-      //console.log(i + ": " + item);
-    })
-    
     $(".mw-body .mw-body-content .mw-parser-output .wikitable tbody tr td").each((i,el) => {
       const item = $(el).html();
       if(numbers.length < 20 && !isNaN(parseFloat(item))){
@@ -22,7 +18,15 @@ request("https://en.wikipedia.org/wiki/List_of_most-streamed_artists_on_Spotify"
       }
       //console.log(i + ": " + item);
     })
-    console.log(numbers);
+     $(".mw-body .mw-body-content .mw-parser-output .wikitable tbody td a").each((i,el) => {
+      const item = $(el).text().replace(/[0-9]/g, '').replace(/[\[\]']+/g,'');
+      if(stats.length < 20 && item != ""){
+        stats.push(item);
+      }
+      //console.log(i + ": " + item);
+    })
+    
+    console.log(stats, numbers)
   }
   else{
     console.log("error")
